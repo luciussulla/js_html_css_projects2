d210869f2535cc3b95574d09c3511d5d265712f1
luciussulla <maciek.kowalczyk@yandex.com>
2022-10-30 20:58:04 +0100

    show hide sidebar sub menu

--- sidebarMenu/script.js
+++ sidebarMenu/script.js
@@ -1,3 +1,10 @@
 document.querySelector('.hamburger-menu').addEventListener('click', ()=> {
   document.querySelector('.container').classList.toggle('open-sidebar'); 
+}); 
+
+[...document.querySelectorAll('.nav-link')].forEach((elem)=> {
+  elem.addEventListener('click', (e)=> {
+    // console.log(e.target.parentNode); 
+    e.target.parentNode.parentNode.classList.toggle('open-sub-nav-list');  
+  })
 })
