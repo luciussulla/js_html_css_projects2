83ecbbc2a5b40768cccf3a77d48487baa80b39e5
luciussulla <maciek.kowalczyk@yandex.com>
2022-11-17 19:55:25 +0100

    styles for form, buttons, and disappearing heading effect

---
+++ 39_form_validation/script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('.container'); 
+const headingSpan2 = document.querySelector('.heading-span-2'); 
+
+document.querySelector('.signup-btn').addEventListener('click', ()=> {
+  container.classList.add('change'); 
+  setTimeout(()=>   headingSpan2.textContent = 'Up' ,200); 
+
+})
+
+document.querySelector('.signin-btn').addEventListener('click', ()=> {
+  container.classList.remove('change'); 
+  setTimeout(()=>   headingSpan2.textContent = 'In' ,200); 
+})
+
+
+
