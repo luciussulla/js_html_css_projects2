c1d89f2ab82356f8a3a1f289f0339d8ff8cc710a
luciussulla <maciek.kowalczyk@yandex.com>
2023-02-04 17:54:20 +0100

    Add project 40 with slideshow

---
+++ 40_social_slideshow/script.js
@@ -0,0 +1,17 @@
+const slideshow = document.querySelector('.slideshow'); 
+
+setInterval(()=> {
+
+  const firstIcon = slideshow.firstElementChild; 
+  firstIcon.classList.add('faded-out'); 
+
+  setTimeout(()=> {
+    slideshow.removeChild(firstIcon); 
+    slideshow.appendChild(firstIcon); 
+    setTimeout(()=> {
+      firstIcon.classList.remove('faded-out');
+    },500); 
+  }, 500); 
+  
+}, 3000); 
+
