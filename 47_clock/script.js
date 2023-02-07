17c6d307fd6fb43aa895435c3326b076f2d5d654
luciussulla <maciek.kowalczyk@yandex.com>
2023-02-07 21:07:41 +0100

    Finish the 47_clock project

---
+++ 47_clock/script.js
@@ -0,0 +1,22 @@
+const hour   = document.querySelector('.hour'); 
+const minute = document.querySelector('.minute'); 
+const second = document.querySelector('.second'); 
+
+
+function setDate() {
+  const now = new Date(); 
+  const getSecond = now.getSeconds(); 
+  const getMinute = now.getMinutes(); 
+  const getHour   = now.getHours(); 
+
+  const secondDegree = (getSecond / 60) * 360;
+  second.style.transform = `rotate(${secondDegree}deg)`; 
+
+  const minutesDegree = (getMinute / 60) * 360;
+  minute.style.transform =  `rotate(${minutesDegree}deg)`; 
+
+  const hoursDegree = (getHour / 24) * 360;
+  hour.style.transform = `rotate(${hoursDegree}deg)`; 
+}
+
+setInterval(setDate, 1000); 
