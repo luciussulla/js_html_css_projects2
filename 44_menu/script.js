e1f5a7adadce658fdbc6fd88d92ee21cad8cd068
luciussulla <maciek.kowalczyk@yandex.com>
2023-02-07 10:03:54 +0100

    Completeproject 44 with menu

---
+++ 44_menu/script.js
@@ -0,0 +1,12 @@
+const links = document.querySelectorAll('.navbar-link'); 
+
+links.forEach((link)=> {
+  link.addEventListener('click', (e)=> {
+    e.preventDefault()
+    links.forEach(link=> link.className = "navbar-link"); 
+    link.classList.add('change');
+    link.previousElementSibling.classList.add('prevElChange'); 
+    link.nextElementSibling.classList.add('nextElChange');
+  })
+})
+
