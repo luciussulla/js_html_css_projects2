e49630d67b8bc9335fdbe305406fbb704ebce67f
luciussulla <maciek.kowalczyk@yandex.com>
2023-02-08 10:47:13 +0100

    FInish 46 jumping square project

--- 47_clock/script.js
+++ 47_clock/script.js
@@ -3,11 +3,14 @@ const minute = document.querySelector('.minute');
 const second = document.querySelector('.second'); 
 
 
+
 function setDate() {
   const now = new Date(); 
   const getSecond = now.getSeconds(); 
   const getMinute = now.getMinutes(); 
   const getHour   = now.getHours(); 
+
+  console.log(getHour, getMinute, getSecond); 
 
   const secondDegree = (getSecond / 60) * 360;
   second.style.transform = `rotate(${secondDegree}deg)`; 
@@ -15,7 +18,7 @@ function setDate() {
   const minutesDegree = (getMinute / 60) * 360;
   minute.style.transform =  `rotate(${minutesDegree}deg)`; 
 
-  const hoursDegree = (getHour / 24) * 360;
+  const hoursDegree = (getHour / 12) * 360;
   hour.style.transform = `rotate(${hoursDegree}deg)`; 
 }
 
